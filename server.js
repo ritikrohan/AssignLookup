de1ca96c516c9ab2cb434e4af0a597577db59221
RITIK ROHAN <ritikrohan@RITIKs-MacBook-Air.local>
2025-03-18 17:19:31 +0530

    Add cors

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const { getValueFromLookup } = require('./lookup');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(cors()); // Enable CORS
 app.use(express.json());
 
 app.get('/lookup/:key', (req, res) => {
