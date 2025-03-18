be08b06754ce33607a503ca62a755dc7a8c3879a
RITIK ROHAN <ritikrohan@RITIKs-MacBook-Air.local>
2025-03-18 15:18:41 +0530

    refactoring

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { getValueFromLookup } = require('./data/lookup');
+const { getValueFromLookup } = require('./lookup');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
